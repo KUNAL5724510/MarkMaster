fe3b5be5821d0448e4dd8b4232b634c8764c1b80
KUNAL SHARMA <kunal5724510@gmail.com>
2025-04-19 07:07:18 +0530

    add student completed

--- src/main/java/MarkMaster/com/MarkMaster/Entity/Student.java
+++ src/main/java/MarkMaster/com/MarkMaster/Entity/Student.java
@@ -3,12 +3,14 @@ package MarkMaster.com.MarkMaster.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "student")
 public class Student {
    
+    @Id
     @GeneratedValue
     @Column(name = "id")
     private int id;
